23fa65ed17b10416c107f75fbdad8561e4cc47c4
perkasa tricahya putra dewa patrikha <perkaspatrikha123@gmail.com>
2026-04-10 10:25:11 +0700

    Update Program.cs

--- NilaiSiswaApp/Program.cs
+++ NilaiSiswaApp/Program.cs
@@ -1,15 +1,1 @@
-﻿int nilai;
-string nama;
-
-Console.WriteLine("Aplikasi Nilai Siswa");
-
-Console.Write("Input Nama: ");
-nama = Console.ReadLine();
-
-Console.Write("Input Nilai: ");
-nilai = int.Parse(Console.ReadLine());
-
-if (nilai >= 75)
-    Console.WriteLine("Selamat Anda Lulus");
-else
-    Console.WriteLine("Maaf Anda Gagal");
+﻿
